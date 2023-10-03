bc98562b4427e91d5e547f744013746882fc4fbf
Nabi <g.nabi@shadhinbd.com>
2023-10-03 20:47:02 +0600

    Modified users migration file

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,11 +14,14 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email')->unique();
+            $table->string('phone', 20)->unique();
+            $table->string('email')->unique()->nullable();
+            $table->foreignId('user_type_id');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
