4e891a959dff4cc22c9a4387d41f85085782c775
Nabi <g.nabi@shadhinbd.com>
2024-02-19 00:08:43 +0600

    Added formatResponse in the main controller file which return json response.

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -9,4 +9,14 @@ use Illuminate\Routing\Controller as BaseController;
 class Controller extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
+
+    public function formatResponse(int $error, int $code, string $msg, mixed $data): object
+    {
+        return response()->json([
+            'error' => $error,
+            'code' => $code,
+            'msg' => $msg,
+            'data' => $data,
+        ]);
+    }
 }
